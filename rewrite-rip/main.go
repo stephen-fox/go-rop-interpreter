ac21cef2c8552ced4fd92b95da9d320d4c46d04d
Stephen Fox <stephen.j.fox.jr@gmail.com>
2024-04-29 21:04:27 -0400

    rewrite-rip: Find addrs that look like RIP.

--- rewrite-rip/main.go
+++ rewrite-rip/main.go
@@ -10,11 +10,22 @@ import (
 	"unsafe"
 )
 
+var (
+	uncalledPtr = reflect.ValueOf(Uncalled).Pointer()
+)
+
 //go:noinline
 func main() {
 	log.Println("pid:", os.Getpid())
 
-	junk_x86()
+	switch runtime.GOARCH {
+	case "arm64":
+		junk_arm()
+	case "amd64":
+		junk_x86()
+	default:
+		log.Fatalf("unsupported arch: %q", runtime.GOARCH)
+	}
 
 	log.Println("nope")
 }
@@ -25,17 +36,11 @@ func junk_arm() {
 
 	pc, _, line, ok := runtime.Caller(1)
 	if !ok {
-		log.Fatalln("nope")
-	}
-
-	for pc%4 != 0 {
-		pc++
+		log.Panicln("nope")
 	}
 
 	_localVarPtr := unsafe.Pointer(&b)
 	localVarPtrBack := unsafe.Pointer(uintptr(_localVarPtr) - 512)
-
-	uncalledPtr := reflect.ValueOf(Uncalled).Pointer()
 
 	log.Printf("pc: 0x%x | pc line: %d | main: 0x%x | uncalled: 0x%x",
 		pc, line, reflect.ValueOf(main).Pointer(), uncalledPtr)
@@ -53,13 +58,13 @@ func junk_arm() {
 		fmt.Fprintf(os.Stdout, "0x%x: 0x%x - 0x%x\n",
 			memoryPtr, value, valueLittle)
 
-		if valueLittle == pc {
+		if isAddressNear(valueLittle, pc) {
 			binary.LittleEndian.PutUint32(stackMemory[i:], uint32(uncalledPtr))
 
-			log.Printf("found pc 0x%x at 0x%x | new value is: 0x%x",
-				pc, memoryPtr, stackMemory[i:i+4])
+			log.Printf("found possible saved rip 0x%x at 0x%x | new value is: 0x%x",
+				valueLittle, memoryPtr, stackMemory[i:i+4])
 
-			return
+			//return
 		}
 
 		memoryPtr += 4
@@ -74,31 +79,22 @@ func junk_x86() {
 
 	pc, _, line, ok := runtime.Caller(1)
 	if !ok {
-		log.Fatalln("nope")
-	}
-
-	for pc%4 != 0 {
-		log.Printf("add (pc was: 0x%x", pc)
-		pc++
+		log.Panicln("nope")
 	}
 
 	_localVarPtr := unsafe.Pointer(&b)
-	localVarPtrBack := unsafe.Pointer(uintptr(_localVarPtr) - 64)
 
-	uncalledPtr := reflect.ValueOf(Uncalled).Pointer()
-
-	log.Printf("pc: 0x%x | pc line: %d | main: 0x%x | uncalled: 0x%x",
+	log.Printf("main: 0x%x | pc line: %d | main: 0x%x | uncalled: 0x%x",
 		pc, line, reflect.ValueOf(main).Pointer(), uncalledPtr)
 
+	localVarPtrBack := unsafe.Pointer(uintptr(_localVarPtr) - 1024)
+
 	// func Float32bits(f float32) uint32 { return *(*uint32)(unsafe.Pointer(&f)) }
-	stackMemory := *(*[1024]byte)(localVarPtrBack)
+	stackMemory := *(*[2048]byte)(localVarPtrBack)
 
 	memoryPtr := uintptr(localVarPtrBack)
 
-	for i := 0; i < len(stackMemory); i += 8 {
-		//log.Printf("i: %d | 0x%x(memPtr) + 0x%x(i) = 0x%x",
-		//	i, memoryPtr, i, memoryPtr+8)
-
+	for i := 0; i < 2048; i += 8 {
 		value := stackMemory[i : i+8]
 
 		valueLittle := uintptr(binary.LittleEndian.Uint64(value))
@@ -106,19 +102,28 @@ func junk_x86() {
 		fmt.Fprintf(os.Stdout, "0x%x: 0x%x - 0x%x\n",
 			memoryPtr, value, valueLittle)
 
-		if valueLittle == pc {
+		//if valueLittle == 0x4142434445464748 {
+		//	log.Println("TODO")
+		//	binary.LittleEndian.PutUint64(stackMemory[i:], uint64(uncalledPtr))
+		//}
+
+		if isAddressNear(valueLittle, pc) {
 			binary.LittleEndian.PutUint64(stackMemory[i:], uint64(uncalledPtr))
 
-			log.Printf("found pc 0x%x at 0x%x | new value is: 0x%x",
-				pc, memoryPtr, stackMemory[i:i+8])
-
-			return
+			log.Printf("found possible saved rip 0x%x at 0x%x | new value is: 0x%x",
+				valueLittle, memoryPtr, stackMemory[i:i+8])
 		}
 
 		memoryPtr += 8
 	}
 
+	//log.Printf("b: 0x%x", b)
+
 	fmt.Scanln()
+}
+
+func isAddressNear(addr uintptr, target uintptr) bool {
+	return addr > target && addr < target+1024
 }
 
 //go:noinline

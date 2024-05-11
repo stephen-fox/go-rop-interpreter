55512aa01ff925fb39a6156856c8e9c817f3e159
SeungKang <13smk@queensu.ca>
2024-05-10 22:31:53 -0400

    injector: make sure function is always the same size

--- injector/main.go
+++ injector/main.go
@@ -94,10 +94,18 @@ func mainWithError() error {
 
 	log.Printf("size: %d | location: 0x%x", sym.Size, sym.Location)
 
-	if len(ropGadgets) != int(sym.Size) {
+	log.Printf("TODO: rop gadget size: %d", len(ropGadgets))
+
+	if len(ropGadgets) > int(sym.Size) {
 		return fmt.Errorf("rop gadgets size must be %d bytes to overwrite function",
 			sym.Size)
 	}
+
+	for len(ropGadgets) < int(sym.Size) {
+		// pad the end with NOPs
+		ropGadgets = append(ropGadgets, 0x90)
+	}
+	log.Printf("TODO: rop gadget size: %d", len(ropGadgets))
 
 	_, err = f.Seek(0, io.SeekStart)
 	if err != nil {

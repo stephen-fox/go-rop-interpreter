69e0758a11f8a31423b5e9a3a5a8db62fef2e217
Stephen Fox <stephen.j.fox.jr@gmail.com>
2024-08-25 09:21:43 -0400

    rop-interpreter: Added Go doc to compiler.

--- rop-interpreter/compiler/main.go
+++ rop-interpreter/compiler/main.go
@@ -1,7 +1,61 @@
-// compiler reads the rop chain file and translates it into a slice of byte to
-// input into rop-runner to execute a rop chain. If a rop gadget is written as
-// string, it checks if this gadget is in the rop gadgets binary and overwrites
-// the string with the address to the gadget.
+// compiler attempts to ease the difficulty of writing ROP chains by
+// compiling a human readable "source" format into a binary ROP chain.
+// compiler writes the resulting "unresolved ROP chain" to stdout.
+//
+// # How it works
+//
+// Compilation is accomplished by first parsing two pieces of data:
+//
+//   - A ROP chain source file
+//   - A binary file containing ROP gadgets. This must be injected
+//     into the runner program using the injector program (this file
+//     can be generated using the "nasm" assembler, refer to the
+//     examples directory)
+//
+// ROP gadgets referenced in the source file are looked-up in the specified
+// ROP chain binary file. The "chain" file should match the data that was
+// written to the runner program using the injector program.
+//
+// The compiler translates the source file into a payload known as the
+// "unsresolved ROP chain" which is interpreted by the runner program
+// at runtime.
+//
+// While we assume the runner program is written in Go, there is (theoretically)
+// nothing stopping us from writing the the runner in a different programming
+// language. There is no contract between the compiler and the runner beyond
+// resolving gadget offsets into addresses.
+//
+// # Source file format
+//
+// The ROP chain source file consists of newline delimited strings. Each line
+// starts with an identifier string followed by arguments in the format of:
+//
+//	<identifier-string> <argument>
+//
+// Possible identifier strings consists of the following:
+//
+//   - g: ROP gadget in human-readable assembly format
+//   - d: A blob of arbitrary, hex-encoded data (automatically
+//     left-padded with zeros)
+//   - D: Same as "d", but no zero padding is performed
+//
+// # Output file format
+//
+// The compiler produces a blob of binary data that we refer to as the
+// "unresolved ROP chain". It is "unresolved" because each gadget is
+// represented by its offset within the ROP gadget file. We use offsets
+// because gadgets' memory addresses cannot be known ahead of time due
+// to PIE and ASLR. Thus, the runner program must resolve these offsets
+// into memory addresses when it parses the unresolved ROP chain.
+//
+// The runner program assumes that the first instruction in the chain
+// is a "ret" instruction. This begins execution of the ROP chain.
+//
+// # Future work
+//
+// We can potentially make this less error-prone by reading the available
+// ROP gadgets from the runner program. We did not pursue this because
+// we were unsure how Go programs may be structured in the future.
 package main
 
 import (
